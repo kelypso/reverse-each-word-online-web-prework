56facb867c6a9f39f98f2aa001d56ecbdcc17f92
Kelsey Low <kklowlow@gmail.com>
2019-01-05 17:08:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence)
   new_arr = sentence.split(" ")
   reverse_array = []
-  new_arr.each do |word|
-    word = word.reverse
+  reverse_array = new_arr.each { |word| word.reverse }
     reverse_array << word.join(" ")
     puts reverse_array
   end
 end
+
