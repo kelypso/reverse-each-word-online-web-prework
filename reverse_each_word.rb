1a211072ed86feab6b31ca2c2b5b2e53ade2bc12
Kelsey Low <kklowlow@gmail.com>
2019-01-05 17:04:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,15 @@
-def reverse_each_word(string)
-  new_arr = string.split(" ")
+def reverse_each_word(sentence)
+  new_arr = sentence.split(" ")
   reverse_array = []
-  new_arr.each do |word|
-    word = word.reverse
+  reverse_array = new_arr.each do ==|word| word.reverse
     reverse_array = word.join(" ")
     puts reverse_array
   end
 end
 
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
